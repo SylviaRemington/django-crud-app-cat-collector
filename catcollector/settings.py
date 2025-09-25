eef98f6e1503a5c6ec160b9519b2f4100574ca7a
SylviaRemington <sylviaremington@gmail.com>
2025-09-25 10:48:33 -0700

    installing django urls and views - the section before migrating

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # add main_app here
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
