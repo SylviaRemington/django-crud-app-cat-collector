45c8a82a423fc1818f44655060de025ed969c2d6
SylviaRemington <sylviaremington@gmail.com>
2025-09-28 09:47:22 -0700

    addtl notes

--- main_app/views.py
+++ main_app/views.py
@@ -9,10 +9,12 @@ def home(request):
     # Send a simple HTML response
     # This is the view function responding to your http resquest. 
     # HttpResponse object is the simplest way to return content in Django.
+    # Home page - Send a simple html response
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 # Define an about view function
 def about(request):
+    # About page
     return HttpResponse('<h1>About Me:</h1><h2>My name is Sylvia and I love to dance!</h2>')
 
 
