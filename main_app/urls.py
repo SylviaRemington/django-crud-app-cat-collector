96f245719a02e722daa9018efe7380ce36440f77
SylviaRemington <sylviaremington@gmail.com>
2025-09-27 18:59:41 -0700

    addtl notes regarding urlpatterns and how it is similar to routes in Express

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
     # Routes will be added here
+    # The urlpatterns list is where you specify each route and define it similar to 
+    # how routes are defined/grouped in controllers in Express
 ]
